abfa1a563ea90fabb59f7450db8779e7a069ecbb
sm1215 <sm1215@gmail.com>
2018-04-22 09:30:17 -0400

    project scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { GameMenuComponent } from './game-menu/game-menu.component';
+import { GameControllerComponent } from './game-controller/game-controller.component';
+import { GameBoardComponent } from './game-controller/game-board/game-board.component';
+import { CellComponent } from './game-controller/game-board/cell/cell.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameMenuComponent,
+    GameControllerComponent,
+    GameBoardComponent,
+    CellComponent
   ],
   imports: [
     BrowserModule

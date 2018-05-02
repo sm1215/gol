a1bb79e8fe58d1b5298a28a7956b962c87064cd7
Sam Miller <sm1215@gmail.com>
2018-05-01 21:56:08 -0400

    a little more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+const GAMESTATE = [
+  'PAUSED',
+  'RUNNING',
+  'END'
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  gameState = GAMESTATE[0];
+
+  onGameState(newState: string) {
+    switch (newState) {
+      case 'pause':
+        this.gameState = GAMESTATE[0];
+      break;
+      case 'start':
+        this.gameState = GAMESTATE[1];
+      break;
+      case 'clear':
+        this.gameState = GAMESTATE[0];
+      break;
+      case 'random':
+        this.gameState = GAMESTATE[0];
+      break;
+      case 'end':
+        this.gameState = GAMESTATE[2];
+      break;
+    }
+
+    console.log('this.gameState', this.gameState);
+  }
 }
--- src/app/game-menu/game-menu.component.ts
+++ src/app/game-menu/game-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-game-menu',
@@ -6,15 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-menu.component.css']
 })
 export class GameMenuComponent implements OnInit {
-
+  // https://medium.com/dailyjs/3-ways-to-communicate-between-angular-components-a1e3f3304ecb
+  @Output() gameState: EventEmitter<null> = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
   }
 
-  onGameControl(arg) {
-    console.log('arg', arg);
-    
+  onGameControl(gameState: string) {
+    console.log('Game Menu: onGameControl gameState', gameState);
   }
 
 }

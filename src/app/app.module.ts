96acd170f2a59dfca1b31e15c75ffe8e3cec0bc8
sm1215 <sm1215@gmail.com>
2018-05-15 12:25:38 -0400

    removing some unnecessary complexity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GameMenuComponent } from './game-menu/game-menu.component';
-import { GameControllerComponent } from './game-controller/game-controller.component';
-import { GameBoardComponent } from './game-controller/game-board/game-board.component';
-import { CellComponent } from './game-controller/game-board/cell/cell.component';
+import { GameBoardComponent } from './game-board/game-board.component';
+import { CellComponent } from './game-board/cell/cell.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     GameMenuComponent,
-    GameControllerComponent,
     GameBoardComponent,
     CellComponent
   ],
--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -6,11 +6,23 @@ import { Component, OnInit, OnChanges, Input } from '@angular/core';
   styleUrls: ['./game-board.component.css']
 })
 export class GameBoardComponent implements OnInit, OnChanges {
-  @Input()
-  boardBounds: {
+  _gameState: string;
+  _boardBounds: any = {
     height: 0,
     width: 0
   };
+
+  @Input()
+  set gameState(value: string) {
+    this._gameState = value;
+    console.log('[GameBoard set gameState]', this._gameState);
+  }
+
+  @Input()
+  set boardBounds(value: any) {
+    this._boardBounds = value;
+    console.log('[GameBoard set boardBounds]', this._boardBounds);
+  }
 
   gameboard = [];
 
@@ -22,13 +34,14 @@ export class GameBoardComponent implements OnInit, OnChanges {
 
   ngOnChanges() {
     this.setupGameboard();
+    console.log('[GameBoard OnChanges] setting up gameboard');
   }
 
   setupGameboard() {
     const gameboard = [];
-    for (let y = 0; y < this.boardBounds.height; y++) {
+    for (let y = 0; y < this._boardBounds.height; y++) {
       const row = [];
-      for (let x = 0; x < this.boardBounds.width; x++) {
+      for (let x = 0; x < this._boardBounds.width; x++) {
         row.push(x);
       }
       gameboard.push(row);

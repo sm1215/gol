f17fd963fe8f532f5a4f9585713ff6105c331142
sm1215 <sm1215@gmail.com>
2018-05-18 17:33:06 -0400

    elaborated on datastore service

--- src/app/datastore.service.ts
+++ src/app/datastore.service.ts
@@ -1,21 +1,57 @@
 import { Injectable } from '@angular/core';
 
+const STORAGE_KEY = 'GoL';
+
 @Injectable()
+
+
+// TODO: Build some tests for this service
 export class DatastoreService {
 
   constructor() { }
 
+  // Don't want to overwrite everything, just append to it
+  // use retrieveGoL first, it should return an array that can be added to
   saveGame(data: object[]) {
-    console.log('[DatastoreService Save Game]', data);
-
-    const stringified = JSON.stringify(data);
-    localStorage.setItem('GoL', stringified);
+    const golData = this.retrieveGol();
+    golData.push(data);
+    this.storeGol(golData);
   }
 
-  loadGame() {
-    const stringified = localStorage.getItem('GoL');
-    const data = JSON.parse(stringified);
-    return data;
+  // Try to load a requested game or default to latest save
+  loadGame(requestedIndex?: number) {
+    const golData = this.retrieveGol();
+    const gameIndex = requestedIndex ? requestedIndex : golData.length - 1;
+    if (golData[gameIndex] !== undefined) {
+      return golData[gameIndex];
+    }
+  }
+
+  // The app should store everything under a single key
+  // Need to stringify data first
+  storeGol(data) {
+    if (window.localStorage) {
+      const stringified = JSON.stringify(data);
+      localStorage.setItem(STORAGE_KEY, stringified);
+    } else {
+      return false;
+    }
+  }
+
+  // This handles retrieving that key and parsing the JSON
+  retrieveGol() {
+    if (window.localStorage) {
+      const stringified = localStorage.getItem(STORAGE_KEY);
+      let data;
+      if (!stringified) {
+        data = [];
+      } else {
+        data = JSON.parse(stringified);
+      }
+      return data;
+    } else {
+      return false;
+    }
   }
 
 }
